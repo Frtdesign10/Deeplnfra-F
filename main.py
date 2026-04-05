9334e63efebd49357de814364786d64fbaf62c60
Frtdesign10 <sfferhat123@gmail.com>
2026-04-06 00:57:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from kivy.uix.button import Button
 
 class MainApp(App):
     def build(self):
-        # En basit buton: Tıklayınca metni değişir
-        return Button(text="Hello from Mardin! Click me.", on_release=self.change_text)
-
-    def change_text(self, instance):
-        instance.text = "Build Successful!"
+        return Button(text="Success! APK is Working.")
 
 if __name__ == "__main__":
     MainApp().run()
